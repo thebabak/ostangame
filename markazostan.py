8746bfba86517f83540d0c2830f25ed18b6616aa
Babak <babak.bayanian@gmail.com>
2023-10-04 09:48:25 +0330

    change

--- markazostan.py
+++ markazostan.py
@@ -2,11 +2,6 @@
 from turtle import Turtle
 import pandas
 
-
-
-
-
-
 class MarkazOstan:
     def __init__(self,R1):
         self.positionOstan=[]
@@ -14,12 +9,14 @@ class MarkazOstan:
 
     def create_MARKAZ(self,R2):
         for position in R2:
-            self.add_segment(position)
+            self.add_circle(position)
             
-    def add_segment(self,position):
+    def add_circle(self,position):
         shahr=Turtle("circle")
-        shahr.color("white")
         shahr.penup()
+
+        shahr.color("white")
+        shahr.shapesize(.5)
         shahr.goto(position)
 
 ostan_CSV=pandas.read_csv("ostan.csv")# data
